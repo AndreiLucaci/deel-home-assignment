75fcb1644132f57ae53259b5a8698aa0c84cf9b8
Andrei Lucaci <lucaciandrei@outlook.com>
2024-05-30 18:54:20 +0300

    chore: fixed missing imports for nest module builder on auth spec tests

--- libs/auth/src/auth.service.spec.ts
+++ libs/auth/src/auth.service.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { TokenService } from './token.service';
+import { UserService } from '@app/storage/user.service';
+import { CryptoService } from '@app/utils/crypto/crypto.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        {
+          provide: TokenService,
+          useValue: jest.fn(),
+        },
+        {
+          provide: UserService,
+          useValue: jest.fn(),
+        },
+        {
+          provide: CryptoService,
+          useValue: jest.fn(),
+        },
+        AuthService,
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
