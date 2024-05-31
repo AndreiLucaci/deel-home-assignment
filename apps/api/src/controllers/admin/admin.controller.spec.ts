9f03618c195d7ba919ee7316bc0dcf8f49b3c9fc
Andrei Lucaci <lucaciandrei@outlook.com>
2024-05-31 11:50:37 +0300

    chore: added fixes for missing imports in admin controller tests

--- apps/api/src/controllers/admin/admin.controller.spec.ts
+++ apps/api/src/controllers/admin/admin.controller.spec.ts
@@ -1,11 +1,33 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AdminController } from './admin.controller';
+import { DeelSeeder } from '@app/storage/seeder/deel.seeder';
+import { AuthService } from '@app/auth';
+import { AuthGuard } from '../../guards/auth.guard';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AdminController', () => {
   let controller: AdminController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: JwtService,
+          useValue: jest.fn(),
+        },
+        {
+          provide: DeelSeeder,
+          useValue: jest.fn(),
+        },
+        {
+          provide: AuthService,
+          useValue: jest.fn(),
+        },
+        {
+          provide: AuthGuard,
+          useValue: jest.fn(),
+        },
+      ],
       controllers: [AdminController],
     }).compile();
 
